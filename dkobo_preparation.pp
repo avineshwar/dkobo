8f53f60f2d3920ab69373fe9ba73fd4673481d5e
Avineshwar Pratap Singh <asingh16@stevens.edu>
2017-03-29 18:46:03 -0400

    Update dkobo_preparation.pp

--- dkobo_preparation.pp
+++ dkobo_preparation.pp
@@ -21,7 +21,7 @@ package { 'nodejs' :
 	require => Exec['nodejs_1']
 }
 
-# Since this is a repeatable task which won't be skipped, we will be keeping such items commented out and only execut manifest files in a specific order to avoid repeats.
+# Since this is a repeatable task which won't be skipped, we will be keeping such items commented out and only execute manifest files in a specific order to avoid repeats.
 exec { 'machine_prep' :
 	command => '/bin/rm --force /etc/timezone > /dev/null 2>&1 && /bin/echo "America/New_York" > /etc/timezone && /bin/systemctl restart cron.service'
 }
